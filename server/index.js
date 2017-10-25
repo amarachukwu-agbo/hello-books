3102bc85817293a3165cab078c4d7d42daa463f8
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2017-10-25 23:29:40 +0100

    Add routes to views

--- server/index.js
+++ server/index.js
@@ -2,18 +2,73 @@
 import express from 'express';
 import jsonfile from 'jsonfile';
 import bodyParser from 'body-parser';
+import path from 'path';
 import User from './models/User';
 import Admin from './models/Admin';
+
+
 
 const app = express();
 
 // Set up middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/../public')));
+app.set('views', path.join(__dirname, '/../templates'));
+app.set('view engine', 'ejs');
 
-// Configure server to listen on port 3000
-const port = process.env.PORT || 4000;
+// Configure server to listen on port 8000
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// Specify routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/bookdetails', (req, res) => {
+  res.render('bookdetails');
+});
+
+app.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+app.get('/admin', (req, res) => {
+  res.render('admin');
+});
+
+app.get('/addbook', (req, res) => {
+  res.render('addbook');
+});
+
+app.get('/borrow', (req, res) => {
+  res.render('borrow');
+});
+
+app.get('/borrowrequests', (req, res) => {
+  res.render('borrowrequests');
+});
+
+app.get('/returnrequests', (req, res) => {
+  res.render('returnrequests');
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile');
+});
+
+app.get('/returnbook', (req, res) => {
+  res.render('returnbook');
+});
+
+app.get('/favorites', (req, res) => {
+  res.render('favorites');
+});
 
 // API endpoint to get all books
 app.get('/api/books', (req, res) => {
