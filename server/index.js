75272effc06391d3522129d220cdc81c092508db
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2017-11-24 14:59:45 +0100

    Modify index.js to use routes, controllers and database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 // import necessary modules
 import express from 'express';
-import jsonfile from 'jsonfile';
 import bodyParser from 'body-parser';
 import path from 'path';
-import User from './models/User';
-import Admin from './models/Admin';
+import dotenv from 'dotenv';
 
-
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
@@ -17,9 +16,13 @@ app.use(express.static(path.join(__dirname, '/../public')));
 app.set('views', path.join(__dirname, '/../templates'));
 app.set('view engine', 'ejs');
 
+// Import route handlers
+require('./routes')(app);
+
 // Configure server to listen on port 8000
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
+console.log(process.env.JWT_SECRET);
 
 // Specify routes
 app.get('/', (req, res) => {
@@ -68,164 +71,6 @@ app.get('/returnbook', (req, res) => {
 
 app.get('/favorites', (req, res) => {
   res.render('favorites');
-});
-
-// API endpoint to get all books
-app.get('/api/books', (req, res) => {
-  jsonfile.readFile('./data/books.json', (err, data) => {
-    if (err) console.log(err);
-    res.json(data.books);
-  });
-});
-
-// API endpoint to add a book
-app.post('/api/books', (req, res) => {
-  const newAdmin = new Admin();
-  const result = newAdmin.addBook(
-    req.body.id,
-    req.body.title,
-    req.body.author,
-    req.body.description,
-    req.body.imageURL,
-    req.body.subject,
-    req.body.quantity,
-  );
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint to modify a book
-   @params bookId is the book's Id
-*/
-app.put('/api/books/:bookId', (req, res) => {
-  const newAdmin = new Admin();
-  const result = newAdmin.updateBook(
-    req.params.bookId,
-    req.body.title,
-    req.body.author,
-    req.body.description,
-    req.body.imageURL,
-    req.body.subject,
-    req.body.quantity,
-  );
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for a user to upvote a book
-   @params bookId is the book's Id
-*/
-app.post('/api/users/upvote/books/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.upvoteBook(req.params.bookId);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for a user to downvote a book
-   @params bookId is the book's Id
-*/
-app.post('/api/users/downvote/books/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.downvoteBook(req.params.bookId);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint to get books with most upvotes. Fix Blocker
-*/
-app.get('/api/books?sort=upvotes&order=desc', (req, res) => {
-  res.json(port);
-});
-
-/* API endpoint for a user to add a book to favorites
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.post('/api/users/:userId/fav/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.favoriteBook(req.params.userId, req.params.bookId);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint to get a user's favorite books
-   @params userId is the user's Id
-*/
-app.get('/api/users/:userId/favbooks', (req, res) => {
-  const newUser = new User();
-  const result = newUser.getFavoriteBooks(req.params.userId);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for a user to review a book
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.post('/api/users/:userId/review/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.reviewBook(req.params.userId, req.params.bookId, req.body.review);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for a user to send a borrow request
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.post('/api/users/:userId/borrow/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.sendBorrowRequest(
-    req.params.userId,
-    req.params.bookId,
-    req.body.reason,
-    req.body.returnDate,
-    req.body.comments,
-  );
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for a user to send a return request
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.post('/api/users/:userId/return/:bookId', (req, res) => {
-  const newUser = new User();
-  const result = newUser.sendReturnRequest(req.params.userId, req.params.bookId, req.body.comments);
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for an admin to accept or decline a borrow request
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.put('/api/users/:userId/borrow/:bookId', (req, res) => {
-  const newAdmin = new Admin();
-  const result = newAdmin.handleBorrowRequest(
-    req.params.userId,
-    req.params.bookId,
-    req.body.action,
-  );
-  res.status(result.statusCode);
-  res.json(result.message);
-});
-
-/* API endpoint for an admin to accept or decline a return request
-   @params userId is the user's Id
-   @params bookId is the book's Id
-*/
-app.put('/api/users/:userId/return/:bookId', (req, res) => {
-  const newAdmin = new Admin();
-  const result = newAdmin.handleReturnRequest(
-    req.params.userId,
-    req.params.bookId,
-    req.body.action,
-  );
-  res.status(result.statusCode);
-  res.json(result.message);
 });
 
 export default app;
