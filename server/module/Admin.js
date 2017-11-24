335eedd2762700395906ccd342e0894486bbfdfa
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2017-11-24 14:54:23 +0100

    Add Admin and User classes

---
+++ server/module/Admin.js
@@ -0,0 +1,71 @@
+// Import necessary modules
+import models from '../models';
+import Users from '../module/Users';
+
+export default class Admin extends Users {
+  /* Method adds a book
+  @param bookId is the book's id
+  @param title is the book's title
+  @param author is the book's author
+  @param description is the book's description
+  @param imageURL is the book's image URL
+  @param subject is the book's subject
+  @param quantity is the book's quantity
+  */
+  addBook(title, author, description, imageURL, subject, quantity) {
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.imageURL = imageURL;
+    this.subject = subject;
+    this.quantity = quantity;
+
+    return models.Book
+      .create({
+        title,
+        author,
+        description,
+        subject,
+        imageURL,
+        quantity,
+      });
+  }
+
+  /* Method modifies a book
+  @param bookId is the book's id
+  @param title is the book's <title></title>
+  @param author is the book's author
+  @param description is the book's description
+  @param imageURL is the book's image URL
+  @param subject is the book's subject
+  @param quantity is the book's quantity
+  */
+  updateBook(id, title, author, description, imageURL, subject, quantity) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.description = description;
+    this.imageURL = imageURL;
+    this.subject = subject;
+    this.quantity = quantity;
+  }
+
+  /* Method accepts or declines a borrow request
+  @param bookId is the book's id
+  @param action takes in 'Accept' or 'Decline'
+  */
+  handleBorrowRequest(userId, bookId, action) {
+    this.userId = userId;
+    this.bookId = bookId;
+    this.action = action;
+  }
+
+  /* Method accepts or declines a return request
+  @param bookId is the book's id
+  @param action takes in 'Accept' or 'Decline'
+  */
+  handleReturnRequest(userId, bookId, action) {
+    this.userId = userId;
+    this.bookId = bookId;
+    this.action = action;
+}
---
+++ server/module/Users.js
@@ -0,0 +1,66 @@
+// Import necessary modules
+
+export default class Users {
+  /* Method votes up a book
+  @param bookId is used to find the index of the book in the
+  books.json file and the upvote count is incremented by 1 */
+  upvoteBook(bookId) {
+    this.bookId = bookId;
+  }
+
+  /* Method votes down a book
+  @param bookId is used to find the index of the book in the
+  books.json file and the downvote count is incremented by 1 */
+  downvoteBook(bookId) {
+    this.bookId = bookId;
+  }
+
+  /* Method favorites up a book
+  @param bookId is used to find the index of the book in the
+  books.json file and increment favorites count if book is not
+  already a user's favorite.
+  @param userId is used to find the index of the user in users.json
+  file and the bookId is added to user's favorites. */
+  favoriteBook(userId, bookId) {
+    this.bookId = bookId;
+    this.userId = userId;
+  }
+
+  /* Method gets a user's favorite books
+  @param userId is used to find the index of the user in the
+  users.json file and get favorites book if any */
+  getFavoriteBooks(userId) {
+    this.userId = userId;
+  }
+  /* Method lets a user review a book
+  @param userId is used to find the index of the user in the users.json file
+  @param bookId  is used to find the index of the user in the users.json file
+  @param review is the review which is added to reviews of a book */
+  reviewBook(userId, bookId, review) {
+    this.userId = userId;
+    this.bookId = bookId;
+    this.review = review;
+  }
+
+  /* Method lets a user send  a borrow request for a book
+  @param userId is used to find the index of the user in the users.json file
+  @param bookId  is used to find the index of the user in the users.json file
+  @params reason, comments and returnDate are the user's reason, comments and
+   the return date */
+  sendBorrowRequest(userId, bookId, reason, returnDate, comments) {
+    this.userId = userId;
+    this.bookId = bookId;
+    this.reason = reason;
+    this.returnDate = returnDate;
+    this.comments = comments;
+  }
+
+  /* Method lets a user send  a return request for a book
+  @param userId is used to find the index of the user in the users.json file
+  @param bookId  is used to find the index of the user in the users.json file
+  @param comments is the user's comments */
+  sendReturnRequest(userId, bookId, comments) {
+    this.userId = userId;
+    this.bookId = bookId;
+    this.comments = comments;
+}
