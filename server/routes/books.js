a0b9222424575ba1b13896162ee03ed3f737c1db
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2017-11-26 21:52:14 +0100

    Add middleware to verify token

--- server/routes/books.js
+++ server/routes/books.js
@@ -1,10 +1,11 @@
 // import necessary modules
 import express from 'express';
 import books from '../controllers/books';
+import verifyToken from '../controllers/auth/verifyToken';
 
 const router = express.Router();
 
 // Endpoint to add a book
-router.post('/', books.createBook);
+router.post('/', verifyToken, books.createBook);
 
 export default router;
