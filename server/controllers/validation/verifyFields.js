1f2a778d78c3bcc5cfed9f93957de81deb34d3ee
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2017-12-01 19:43:49 +0100

    Add validation middleware for login route

--- server/controllers/validation/verifyFields.js
+++ server/controllers/validation/verifyFields.js
@@ -3,14 +3,14 @@ import Joi from 'joi';
 const userSchema = {
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
-  email: Joi.string().email(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+  email: Joi.string().email().required(),
+  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
 };
 
 const validateUser = (req, res, next) => {
-  const result = Joi.validate(req.body, userSchema, { allowUnknown: true, abortEarly: false });
+  const result = Joi.validate(req.body, userSchema, { allowUnknown: false, abortEarly: false });
   if (result.error) {
-    res.status(400).json({ error: result.error.toString()});
+    res.status(400).json({ msg: 'Signup unsuccessful', error: result.error.toString() });
   }
   next();
 };
