5035505b1dd981f8f27e9e6a7cb06fa7abf1c13b
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-16 15:56:18 +0100

    Change field names to match API\'s signup endpoint field names

--- client/helpers/validations/signup.js
+++ client/helpers/validations/signup.js
@@ -1,31 +1,30 @@
 const validate = values => {
     const errors = {};
-    if (!values.first_name || values.first_name.trim() === ''){
-        errors.first_name = "First Name is required";
+    if (!values.firstName || values.firstName.trim() === ''){
+        errors.firstName = "First Name is required";
     }
-    if (!values.last_name || values.last_name.trim() === ''){
-        errors.last_name = "Last Name is required";
+    if (!values.lastName || values.lastName.trim() === ''){
+        errors.lastName = "Last Name is required";
     }
     if (!values.email || values.email.trim() === ''){
         errors.email = "Email is required";
     }
-    if (!values.password1 || values.password1.trim() === ''){
-        errors.password1 = "Password is required";
+    if (!values.password || values.password.trim() === ''){
+        errors.password = "Password is required";
     }
-    if (values.password1 && values.password1.trim() !== '' && values.password1.length < 7) {
-        errors.password1 = "Password must not be less than 6 characters"
+    if (values.password && values.password.trim() !== '' && values.password.length < 7) {
+        errors.password = "Password must not be less than 6 characters"
     }
-    if (values.password1 && values.password1.trim() !== '' && values.password1.length >= 7 
-        && !new RegExp("^(?=.*[a-z])(?=.*[0-9])").test(values.password1) ) {
-        errors.password1 = "Password must contain at least a letter and a number"
+    if (values.password && values.password.trim() !== '' && values.password.length >= 7 
+        && !new RegExp("^(?=.*[a-z])(?=.*[0-9])").test(values.password) ) {
+        errors.password = "Password must contain at least a letter and a number"
     }
     if (!values.password2 || values.password2.trim() === ''){
         errors.password2 = "Confirm Password is required";
     }
     if (values.password2 && values.password2.trim() !== '' 
-        && values.password1 && values.password1 !== ''
-        && values.password1 !== values.password2) {
-            errors.password1 = "Passwords don't match";
+        && values.password && values.password !== ''
+        && values.password !== values.password2) {
             errors.password2 = "Passwords don't match";
         }
     return errors;
