b524a7b5cb432cebfc0a2fb578d704b17c27ffec
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-16 15:45:49 +0100

    Replace SignUpForm component with SignUpPage container

--- client/components/App.js
+++ client/components/App.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { hot } from 'react-hot-loader';
 
-import SignUpForm from './SignUpForm';
+import SignUpPage from './SignUp/SignUpPage';
 
 class App extends Component {
     render() {
         return(
             <Router history = { this.props.history }>
                 <Switch>
-                    <Route path = '/signup' component = { SignUpForm }/>
+                    <Route path = '/signup' component = { SignUpPage }/>
                 </Switch>
             </Router>
         )
-    }
-    
+    }  
 }
 
 export default hot(module)(App);
