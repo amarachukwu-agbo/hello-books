b40fc9bba94b203c72ab20a031335fc7d99ecc71
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-16 16:00:10 +0100

    Add signUp action reducer to root reducer

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
+import auth from './userSignUp';
 
 const rootReducer = combineReducers({
     form: formReducer,
+    auth,
     routing: routerReducer
 });
 
