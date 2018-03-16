539799bab3b2adf0af7a94ac9489d79e4f985096
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-16 16:03:52 +0100

    Remove SignUpForm component since App container contains component

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import {Router, Route, Switch } from 'react-router-dom';
 import style from '../public/style/style.scss';
-import SignUpForm from '../components/SignUpForm';
 import store, { history } from './store';
 import App from '../components/App';
 
