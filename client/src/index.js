27091c610e7ba2fa5c8df9c26632a5fec6a5be24
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-20 21:38:18 +0100

    Include materialize-css package

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import 'materialize-css';
 import '../public/style/style.scss';
 import store, { history } from './store';
 import App from '../components/App';
-
+import '../helpers/materialize/index';
 
 render(
     <Provider store = { store }>
