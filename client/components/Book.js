ce2bcd07dbd056a52f41462e6195f8fd753f63ec
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-22 13:34:21 +0100

    Link component to book details page

--- client/components/Book.js
+++ client/components/Book.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Book = (props) => {
-  const book = props;
+  const { book } = props;
   return (
         <div className="col s12 m6 l4">
             <div className="card">
                 <div className="card-image">
-                    <img src = { book.image } />
-                    <Link to="/" className="btn-floating halfway-fab waves-effect waves-light small red">
+                    <img src = { book.imageURL } className="responsive-img" />
+                    <Link to= { `/books/${book.id}`} className="btn-floating halfway-fab waves-effect waves-light small red">
                         <i className="material-icons">chevron_right</i>
                     </Link>
                 </div>
