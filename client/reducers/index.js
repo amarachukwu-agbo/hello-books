037d19b5b24848279fa0ee8e39a614882707f7b0
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-22 13:30:43 +0100

    Add reducers for book actions

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,6 +3,7 @@ import { routerReducer } from 'react-router-redux';
 import { reducer as formReducer } from 'redux-form';
 import auth from './userSignUp';
 import login from './login';
+import book from './book';
 import { books, mostUpvotedBooks } from './books';
 
 const rootReducer = combineReducers({
@@ -10,6 +11,7 @@ const rootReducer = combineReducers({
   auth,
   login,
   books,
+  book,
   mostUpvotedBooks,
   routing: routerReducer,
 });
