dfdc638d7080cffeccb25eea6b70ce50aea319c0
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-26 12:26:18 +0100

    Add action to favorite a book

--- client/actions/book.js
+++ client/actions/book.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 import { GET_BOOK_SUCCESS, GET_BOOK_REQUEST, GET_BOOK_FAILURE } from './types';
+import { FAVORITE_REQUEST, FAVORITE_SUCCESS, FAVORITE_FAILURE } from './types';
+
 
 import { apiURL } from './userSignUp';
+
+console.log(localStorage.getItem('userToken'));
+axios.defaults.headers.common.Authorization = `Bearer ${localStorage.getItem('userToken')}`;
 
 const getBookRequest = () => ({
   type: GET_BOOK_REQUEST,
@@ -18,10 +23,11 @@ const getBookFailure = error => ({
 });
 
 
-const getBook = params => (dispatch) => {
+export const getBook = params => (dispatch) => {
   dispatch(getBookRequest());
   return axios.get(`${apiURL}/books/${params}`)
     .then((response) => {
+      console.log(response);
       dispatch(getBookSuccess(response.data.book));
     })
     .catch((error) => {
@@ -41,4 +47,34 @@ const getBook = params => (dispatch) => {
     });
 };
 
-export default getBook;
+const favoriteRequest = () => ({
+  type: FAVORITE_REQUEST,
+});
+
+const favoriteSuccess = favCount => ({
+  type: FAVORITE_SUCCESS,
+  favCount,
+});
+
+const favoriteFailure = error => ({
+  type: FAVORITE_FAILURE,
+  error,
+});
+
+export const favoriteBook = (userId, bookId) => (dispatch) => {
+  dispatch(favoriteRequest());
+  return axios.post(`${apiURL}/users/${userId}/fav/${bookId}`)
+    .then((response) => {
+      console.log(response);
+      dispatch(favoriteSuccess(response.data.book.favCount));
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        dispatch(favoriteFailure(error.response.data.msg));
+      } else {
+        dispatch(favoriteFailure(error.message));
+      }
+    });
+};
+
