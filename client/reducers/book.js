a2e235456867bfb110ad077738fa8505b07e33c2
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-26 12:51:58 +0100

    Add reducer for action to favorite book

--- client/reducers/book.js
+++ client/reducers/book.js
@@ -1,4 +1,9 @@
-import { GET_BOOK_SUCCESS, GET_BOOK_REQUEST, GET_BOOK_FAILURE } from '../actions/types';
+import { GET_BOOK_SUCCESS,
+  GET_BOOK_REQUEST,
+  GET_BOOK_FAILURE,
+  FAVORITE_REQUEST,
+  FAVORITE_SUCCESS,
+  FAVORITE_FAILURE } from '../actions/types';
 
 const initialState = {};
 
@@ -23,6 +28,26 @@ const book = (state = initialState, action) => {
         ...state,
         isFetching: false,
         book: action.book,
+      };
+    }
+    case FAVORITE_REQUEST: {
+      return {
+        ...state,
+        isFavoriting: true,
+      };
+    }
+    case FAVORITE_FAILURE: {
+      Materialize.toast(action.error, 1000);
+      return {
+        ...state,
+        isFavoriting: false,
+      };
+    }
+    case FAVORITE_SUCCESS: {
+      return {
+        ...state,
+        book: { ...state.book, favCount: action.favCount },
+        isFavoriting: false,
       };
     }
     default: {
