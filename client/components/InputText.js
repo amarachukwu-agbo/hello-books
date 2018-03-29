9d8beeaf3ca95c7b4db7fd7d8be4d2869c332152
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-29 16:50:29 +0100

    Add textarea component

--- client/components/InputText.js
+++ client/components/InputText.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-const inputText = ({
-  input, label, type, icon, meta: { touched, error },
+export const InputText = ({
+  input, label, type, defaultValue, icon, meta: { touched, error }, ...rest
 }) => (
     <div>
         <i className="material-icons prefix">{ icon }</i>
         <label className = "flow-text truncate"> { label } </label>
-        <input className = "inputText" {...input} type={type} />
+        <input {...input} type={type} value= { defaultValue } { ...rest } />
         {touched && error && <span className = "error flow-text"> {error} </span>}
     </div>
 );
 
-export default inputText;
+export const TextArea = ({
+  input, label, type, ...rest
+}) => (
+      <div>
+          <label> { label } </label>
+          <textarea {...input} type={type} { ...rest } />
+      </div>
+);
 
