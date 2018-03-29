8c220beeef5e49c967e0b49b7fc0718069d44859
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-29 16:47:18 +0100

    Add handler for borrowing a book

--- client/components/BookDetails.js
+++ client/components/BookDetails.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import checkAuthentication from '../helpers/auth';
 
 class BookDetails extends Component {
@@ -7,6 +8,7 @@ class BookDetails extends Component {
     this.favBook = this.favBook.bind(this);
     this.upvoteBook = this.upvoteBook.bind(this);
     this.downvoteBook = this.downvoteBook.bind(this);
+    this.borrowBook = this.borrowBook.bind(this);
   }
 
   favBook() {
@@ -24,43 +26,50 @@ class BookDetails extends Component {
     this.props.downvoteBook(this.props.user.id, this.props.book.id);
   }
 
+  borrowBook() {
+    checkAuthentication(this.props.isAuthenticated);
+    this.props.borrowBook(this.props.user.id, this.props.book.id);
+  }
+
   render() {
     const { book } = this.props;
     console.log(this.props);
     return (
-            <div className="row card-panel">
-              <div className = "row center"><h4 className="book-header">{ book.title }</h4> </div>
-              <div className = 'divider'></div><br/>
-              <div className="z-index-2">
-                  <div className="col s12 m4 l4" ><img src= {book.imageURL} className="responsive-img book-image"/></div>
-                  <div className="col s12 m8 l8">
-                      <p className="bold black-text">Author: <span> { book.author } </span></p>
-                      <div className = 'divider'></div>
-                      <p className = "bold black-text">Category: <span> { book.subject } </span></p>
-                      <div className = 'divider'></div>
-                      <p className = "bold black-text">Copies Borrowed: <span> { book.borrowCount } </span></p>
-                      <div className = 'divider'></div>
-                      <p className = "bold black-text">Copies Available: <span> { book.quantity } </span></p>
-                      <div className = 'divider'></div>
-                      <p className = "bold black-text">Description: <span> { book.description } </span></p>
-                      <div className = 'divider'></div><br/>
-                      <div>
-                          <button className="btn btn-small red">Borrow Book</button>
-                          <div className="left">
-                          <button className="btn btn-small white teal-text left" onClick = { this.upvoteBook } disabled = { this.props.isUpvoting}>
-                            <i className="material-icons prefix">thumb_up</i><span>{book.upvotes}</span>
-                          </button>
-                          <button className="btn btn-small white teal-text left" onClick = { this.downvoteBook } disabled = { this.props.isDownvoting }>
-                            <i className="material-icons prefix">thumb_down</i><span>{book.downvotes}</span>
-                          </button>
-                          <button className="btn btn-small white teal-text left" disabled = { this.props.isFavoriting } onClick={ this.favBook }>
-                              <i className="material-icons prefix">favorite_border</i><span>{book.favCount}</span>
-                          </button>
-                      </div>
-                      </div>
-                  </div>
+      <div className="row card-panel">
+        <div className="row center"><h4 className="book-header">{book.title}</h4> </div>
+        <div className='divider'></div><br />
+        <div className="row">
+          <div className="col s12 m4 l4" ><img src={book.imageURL} className="responsive-img book-image" /></div>
+          <div className="col s12 m8 l8">
+            <p className="bold black-text">Author: <span> {book.author} </span></p>
+            <div className='divider'></div>
+            <p className="bold black-text">Category: <span> {book.subject} </span></p>
+            <div className='divider'></div>
+            <p className="bold black-text">Copies Borrowed: <span> {book.borrowCount} </span></p>
+            <div className='divider'></div>
+            <p className="bold black-text">Copies Available: <span> {book.quantity} </span></p>
+            <div className='divider'></div>
+            <p className="bold black-text">Description: <span> {book.description} </span></p>
+            <div className='divider'></div><br />
+            <div>
+              <Link to={`/borrow/${book.id}`} book={this.props.book} ><button className="btn btn-small red">
+                Borrow Book
+                          </button></Link>
+              <div className="left">
+                <button className="btn btn-small white teal-text left" onClick={this.upvoteBook} disabled={this.props.isUpvoting}>
+                  <i className="material-icons prefix">thumb_up</i><span>{book.upvotes}</span>
+                </button>
+                <button className="btn btn-small white teal-text left" onClick={this.downvoteBook} disabled={this.props.isDownvoting}>
+                  <i className="material-icons prefix">thumb_down</i><span>{book.downvotes}</span>
+                </button>
+                <button className="btn btn-small white teal-text left" disabled={this.props.isFavoriting} onClick={this.favBook}>
+                  <i className="material-icons prefix">favorite_border</i><span>{book.favCount}</span>
+                </button>
               </div>
+            </div>
           </div>
+        </div>
+      </div>
     );
   }
 }
