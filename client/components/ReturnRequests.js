5433605a04efd9f84a7cb626755042f621540dff
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-03-30 17:07:26 +0100

    Add profile components

---
+++ client/components/ReturnRequests.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const ReturnRequests = (props) => {
+  const { books } = props;
+  if (!books.length) {
+    return (
+        <div className="row center">
+            <p className="grey-text">You have no pending return requests </p>
+        </div>
+    );
+  }
+
+  return (
+        <div className="row">
+            <table class="striped responsive-table">
+                <thead>
+                    <tr>
+                        <th>Book</th>
+                        <th>Author</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { books.map((book, index) =>
+                        <tr key={ index }>
+                            <td> { book.returnRequests.title } </td>
+                            <td> { book.returnRequests.author } </td>
+                            <td> { book.createdAt.split('T')[0] } </td>
+                            <td> <button className="btn-flat btn-small">{ book.status } </button></td>
+                        </tr>)}
+                </tbody>
+            </table>
+        </div>
+  );
+};
+
+export default ReturnRequests;
