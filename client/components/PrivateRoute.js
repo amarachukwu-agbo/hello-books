ee660e068638bbf570623afe6017c3fa46343959
amarachukwu-agbo <amarachukwu.agbo@gmail.com>
2018-04-08 21:24:38 +0100

    Protect authenticated route

---
+++ client/components/PrivateRoute.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Route } from 'react-router';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+/**
+ * Private route to navigate over private routes
+ * If not logged in - goes to login
+ */
+
+class PrivateRoute extends React.Component {
+
+    render() {
+        const {
+            isAuthenticated,
+            component: Component,
+            ...props
+        } = this.props
+        
+        return (
+            <Route
+                {...props}
+                render={props =>
+                    isAuthenticated
+                        ?
+                            <Component {...props} />
+                        : (
+                            <Redirect to={{
+                                pathname: '/login',
+                                state: { from: props.location }
+                            }} />
+                        )
+                }
+            />
+        )
+    }
+}
+
+const mapStateToProps = state => ({
+    ...state.login,
+});
+
+export default connect(mapStateToProps)(PrivateRoute)
